49dd55dc43a72ca4c28f9d87d8fbfd9fc863c771
Tanish <94254649+tanish-1@users.noreply.github.com>
2022-09-18 17:53:29 +0530

    Update and rename README.md to Patten3.java

---
+++ Patten3.java
@@ -0,0 +1,43 @@
+# Hollow---Rhombus
+public class Main
+{
+	public static void main(String[] args) {
+	    int n = 4;
+	    for(int i=1;i<=n;i++){
+	        for(int j = 1; j<=i;j++){
+	            if(j==1||j==i)
+	       	System.out.print("*");
+	       	else
+	       System.out.print(" ");
+	        }
+	         for(int j = 1; j<=2*(n-i)-1;j++){
+	             	System.out.print(" ");
+	         }
+	          for(int j = 1; j<=i;j++){
+	            if(j==1||j==i)
+	       	System.out.print("*");
+	       	else
+	       System.out.print(" ");
+	        }
+	       System.out.println();
+	    }
+	  for(int i=n;i>=1;i--){
+	        for(int j = 1; j<=i;j++){
+	            if(j==1||j==i)
+	       	System.out.print("*");
+	       	else
+	       System.out.print(" ");
+	        }
+	         for(int j = 1; j<=2*(n-i)-1;j++){
+	             	System.out.print(" ");
+	         }
+	          for(int j = 1; j<=i;j++){
+	            if(j==1||j==i)
+	       	System.out.print("*");
+	       	else
+	       System.out.print(" ");
+	        }
+	       System.out.println();
+	    }
+	}
+}
